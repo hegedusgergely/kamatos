395504725f0cf2832b0ce26d692c48cb263f683d
Balázs Máté Petró <petrobalazsmate@gmail.com>
2016-08-20 19:47:59 +0200

    Modified Debtor class.

--- src/Kamatos/Debtor.php
+++ src/Kamatos/Debtor.php
@@ -1,17 +1,104 @@
 <?php
-
 namespace Kamatos;
 
-
-Class Debtor
+/**
+ * Description of Debtor
+ * 
+ * @author Gergely Hegedűs <work@hegedusgergely.com>
+ * @author Balázs Máté Petró <petrobalazsmate@gmail.com>
+ */
+class Debtor
 {
-    protected $name;
+    /**
+     * @var string
+     */
+    private $firstName;
+    
+    /**
+     * @var string
+     */
+    private $lastName;
 
-    protected $debt;
+    /**
+     * @var float
+     */
+    private $debt;
 
-    protected $duration;
+    /**
+     * @var int
+     */
+    private $duration;
 
-    protected $interest;
+    /**
+     * @var int
+     */
+    private $interest;
+    
+    /**
+     * Instantiates a new Debtor object.
+     * 
+     * @param string $firstName The debtor's first name.
+     * @param string $lastName The debtor's last name.
+     * @param float $debt The debt.
+     * @param int $duration The duration.
+     * @param int $interest The interest.
+     */
+    public function __construct($firstName, $lastName, $debt, $duration, $interest)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->debt = $debt;
+        $this->duration = $duration;
+        $this->interest = $interest;
+    }
+    
+    /**
+     * Returns the debtor's first name.
+     * 
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Returns the debtor's last name.
+     * 
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Returns the debt.
+     * 
+     * @return float
+     */
+    public function getDebt()
+    {
+        return $this->debt;
+    }
+
+    /**
+     * Returns the duration.
+     * 
+     * @return int
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Returns the interest.
+     * 
+     * @return int
+     */
+    public function getInterest()
+    {
+        return $this->interest;
+    }
 }
-
-?>
